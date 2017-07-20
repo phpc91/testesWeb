e3c748bac18319029837b9b6328cabab882bb8ca
ph-pc <ph-pc@Note-Ph.home>
2017-07-20 13:27:51 -0300

    início do projeto, criada classe BaseDAO abstrata

---
+++ TestesWeb/src/dao/BaseDAO.java
@@ -0,0 +1,26 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class BaseDAO {
+	
+	//TODO passar admin e senha para properties? cripto?
+	private static String user="admin", passwd="admin123",
+			endpoint="tai-db.cyki8d0w5wwv.sa-east-1.rds.amazonaws.com/database";
+	
+	public BaseDAO() {
+		try{
+			Class.forName("org.mariadb.jdbc.Driver");
+		} catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}
+	}
+	
+	protected static final Connection createConnection() throws SQLException{
+		return DriverManager.getConnection("jdbc:mariadb://"+endpoint+"?user="+user+"&password="+passwd);
+	}
+	
+	//TODO implementar cache aqui -- try cache, else db
+}
