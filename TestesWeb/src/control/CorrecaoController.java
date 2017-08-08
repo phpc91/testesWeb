e278af6aef88bf61082e286e06d5c66b6d821e4c
ph-pc <ph-pc@Note-Ph.home>
2017-08-07 21:15:11 -0300

    controller da correcao de prova

--- TestesWeb/src/control/CorrecaoController.java
+++ TestesWeb/src/control/CorrecaoController.java
@@ -19,14 +19,13 @@ public class CorrecaoController extends HttpServlet {
      */
     public CorrecaoController() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO implementar?
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
@@ -34,7 +33,7 @@ public class CorrecaoController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		// TODO implementar?
 		doGet(request, response);
 	}
 
