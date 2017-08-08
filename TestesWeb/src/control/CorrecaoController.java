74b5c122bdbda58f868619db19573aff2745ea0b
ph-pc <ph-pc@Note-Ph.home>
2017-08-08 18:20:53 -0300

    Início do desenvolvimento da correção das provas

--- TestesWeb/src/control/CorrecaoController.java
+++ TestesWeb/src/control/CorrecaoController.java
@@ -1,11 +1,17 @@
 package control;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.ProvaDAO;
+import entidade.Prova;
 
 /**
  * Servlet implementation class CorrecaoController
@@ -25,15 +31,51 @@ public class CorrecaoController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO implementar?
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		Enumeration<String> parameterNames = request.getParameterNames();
+		PrintWriter p = response.getWriter();
+//		while(parameterNames.hasMoreElements()) {
+//			String nome = parameterNames.nextElement();
+//			p.println(nome);
+//			
+//			String[] valores = request.getParameterValues(nome);
+//			for(int i =0; i<valores.length; i++) {
+//				String valor = valores[i];
+//				p.println("\t"+valor+"\n");
+//			}
+//		}
+		
+		Boolean[] respostas = new Boolean[10];
+		int idProva = Integer.parseInt(request.getParameter("prova_id"));
+		Boolean[] gabarito = ProvaDAO.getInstance().getProvaPorId(idProva).getRespostas();
+		int acertos = 0;
+		
+		for(int i=0; i<respostas.length; i++) {
+			respostas[i] = Boolean.parseBoolean(request.getParameter("resposta"+i)); //pega todas respostas
+			
+			if(respostas[i] == gabarito[i]){
+				acertos++;
+				p.println(acertos);
+			}
+		}
+		
+		float nota = 100 * (acertos/(float)respostas.length); //já pensando em tamanho de prova != 10 questoes
+		p.println("Nota="+nota);
+		request.setAttribute("nota", nota);
+		if(nota >= 80.0) {
+			p.println("Sua nota foi "+nota+". Parabéns, você foi aprovado neste treinamento!");
+			//TODO aprovado no treinamento, exibir relatório com tentativas e respectivas notas até aprovação
+		} else {
+			p.println("Sua nota "+nota+" foi baixa demais. Você foi reprovado.");
+			//TODO reprovado, guardar nota e numero de tentativas do treinamento
+		}
+		
+//		request.getRequestDispatcher("/prova/nota.jsp").forward(request,response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO implementar?
 		doGet(request, response);
 	}
 
